f9e982b6d72a6cddd80b22d5bd40d8775134fcba
Roger Zhang <70820857+rogerz74@users.noreply.github.com>
2022-05-06 09:35:16 -0600

    Add files via upload

---
+++ apps/web/components/queries.ts
@@ -0,0 +1,34 @@
+import { gql } from "@apollo/client";
+
+export const GET_POSTS = gql`
+  query getPosts {
+    getPosts {
+      id
+      content
+    }
+  }
+`;
+
+export const CREATE_POST = gql`
+    mutation CreatePost($content: String!) {
+        createPost(content: $content) {
+            content
+            id
+        }
+    }`;
+
+export const DELETE_POST = gql`
+    mutation DeletePost($deletePostId: ID!) {
+        deletePost(id: $deletePostId) {
+            id
+            content
+        }
+    }`;
+
+export const GET_POST_BY_ID = gql`
+    query GetPostById($getPostId: ID!) {
+        getPostById(id: $getPostId) {
+            id
+            content
+        }
+    }`;
